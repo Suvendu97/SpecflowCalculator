4936d22f28bfa50c5919eca888323361051097ac
[EXT] Suvendu Polley <conspol@partner.waters.com>
2023-11-13 17:13:08 +0530

    code cleanup

--- SpecFlowProject10/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowProject10/StepDefinitions/CalculatorStepDefinitions.cs
@@ -60,50 +60,6 @@ namespace SpecFlowProject10.StepDefinitions
             Assert.AreEqual(expectedResult, result);
         }
 
-
-        /*
-        [When(@": I Select the Mode '([^']*)'")]
-        public void WhenISelectTheMode(string mode)
-        {
-            calculator.EnterMode(mode);
-        }
-
-        [Then(@": I Input (.*)")]
-        public void ThenIInput(int number)
-        {
-            string numb = number.ToString();
-            calculator.EnterNumber(numb);
-        }
-
-        [Then(@": I perform '(.*)' action")]
-        public void ThenIPerformAction(string action)
-        {
-            switch(action)
-            {
-                case "add":
-                    calculator.EnterPlus();
-                    break;
-                case "equal":
-                    calculator.EnterEqual();
-                    break;
-                case "StoreinMemory":
-                    calculator.EnterMPlus();
-                    break;
-                case "Square":
-                    calculator.EnterSquare();
-                    break;
-            }
-        }
-
-        [Then(@": The Result is '(.*)'")]
-        public void ThenTheResultIs(string expectedResult)
-        {
-            var result = CalculatorForm.GetResult();
-            Assert.AreEqual(expectedResult, result);
-        }
-
-        */
-
     }
 }
     
